055cb93ac6a344f38d2d70e3901b133781d5dfa9
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2022-07-28 16:08:30 +0530

    Life-cycle Hooks

--- vue-playlist/src/components/Ninjas.vue
+++ vue-playlist/src/components/Ninjas.vue
@@ -6,6 +6,7 @@
         <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
       </li>
     </ul>
+    <button @click="deleteNinja">Delete Ninja</button>
   </div>
 </template>
 
@@ -21,6 +22,29 @@ export default {
     return {
       
     }
+  },
+  methods: {
+    deleteNinja() {
+      this.ninjas.pop();
+    }
+  },
+  beforeCreate() {
+    alert('beforeCreate');
+  },
+  created() {
+    alert('Created');
+  },
+  beforeMount() {
+    alert('beforeMount');
+  },
+  mounted() {
+    alert('Mounted');
+  },
+  beforeUpdate() {
+    alert('beforeUpdate');
+  },
+  updated() {
+    alert('Updated');
   }
 }
 </script>
