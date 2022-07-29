4f440fb3006e54d363a35c4e4ca8b090fac9bc5d
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2022-07-29 15:03:28 +0530

    Till Slots

--- vue-playlist/src/App.vue
+++ vue-playlist/src/App.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-    <app-header></app-header>
-    <app-ninjas :ninjas="ninjas"></app-ninjas>
-    <app-footer></app-footer>
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>This is some info about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
+import formHelper from './components/formHelper.vue'
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    'form-helper': formHelper
   },
   data() {
     return {
-      ninjas: [
-        { name: 'Ryu', speciality: 'Vue Components', show: false },
-        { name: 'Crystal', speciality: 'HTML Wizardry', show: false },
-        { name: 'Hitoshi', speciality: 'Click Events', show: false },
-        { name: 'Tango', speciality: 'Conditionals', show: false },
-        { name: 'Kami', speciality: 'Webpack', show: false },
-        { name: 'Yoshi', speciality: 'Data Diggin', show: false }
-      ]
+
+    }
+  },
+  methods: {
+    handleSubmit: function () {
+      alert('thanks for submitting');
     }
   }
 }
 </script>
 
 <style>
+
 </style>
