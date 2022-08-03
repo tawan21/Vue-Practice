3c878f26358f58d443d6d65e35db48155c3dfe7a
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2022-08-03 16:18:15 +0530

    Routing with vue-router@2.3.1

--- vue-playlist/src/main.js
+++ vue-playlist/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+});
 
 Vue.filter('snippet', value => {
   return value.slice(0, 100) + '...';
@@ -10,5 +17,6 @@ Vue.filter('snippet', value => {
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
